a0e5ce1722e68f9ba63ac66b6a619f792382f75b
shannonliang312 <shannonliang312@gmail.com>
2017-01-24 15:14:40 +0800

    baby steps

--- BabySteps.js
+++ BabySteps.js
@@ -1,3 +1,5 @@
+/* Write a program that accepts one or more numbers as command-line arguments
+  and prints the sum of those numbers to the console (stdout). */
 var arr = process.argv.slice(2);
 
 console.log(arr.reduce(function(a,b){return (+a)+(+b);},0));
