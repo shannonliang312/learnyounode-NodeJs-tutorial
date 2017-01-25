30cccb79d4256be808e1dac496d3ba3feb76bc3c
shannonliang312 <shannonliang312@gmail.com>
2017-01-25 11:03:25 +0800

    update

--- fileServer.js
+++ fileServer.js
@@ -1,3 +1,13 @@
+/* Write an HTTP server that serves the same text file for each request it
+  receives.
+
+  Your server should listen on the port provided by the first argument to
+  your program.
+
+  You will be provided with the location of the file to serve as the second
+  command-line argument. You must use the fs.createReadStream() method to
+  stream the file contents to the response. */
+
 var http = require('http');
 var fs = require('fs');
 var portNumber = process.argv[2];
